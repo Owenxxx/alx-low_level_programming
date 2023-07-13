5fe682eca6c2c7425222348416d17255e55ceae1
oussamajrh <ravenss,com@gmail.com>
2023-07-13 20:53:26 +0100

    cocex1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main - main block
- * Return 0
+ * Return: 0(success)
  */
-int main(void)
+int main(void);
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
