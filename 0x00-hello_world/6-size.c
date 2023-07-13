f4b7cb2c02e41db1291c1673981944be07d49ef7
oussamajrh <ravenss,com@gmail.com>
2023-07-13 20:39:38 +0100

    cocex1

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - 3aa
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int:%i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)", sizeof(long int));
+	printf("Size of a long long int: 5ibyte(s)", sizeof(long long int))
+	printf("Size of a float: %i byte(s)", sizeof(float));
+	return (0);
+}
+
+
+
+
+
+
