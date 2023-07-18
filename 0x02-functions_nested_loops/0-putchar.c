67edb64e2e31c2e499a44cd0eea9ffb555285349
oussamajrh <ravenss,com@gmail.com>
2023-07-18 17:12:49 +0100

    up date

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,16 +3,18 @@
  *main - Prints "codex0" followed by a new line.
  *Return: always 0.
  */
+	
 int main(void)
 {
-	putchar("_");
-	putchar("p");
-	putchar("u");
-	putchar("t");
-	putchar("c");
-	putchar("h");
-	putchar("a");
-	putchar("r");
-	putchar("\n")
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
 }
