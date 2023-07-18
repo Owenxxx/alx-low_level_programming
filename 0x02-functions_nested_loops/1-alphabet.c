73c6ce9a8b6d062809a15315b4c8f50e870cbfee
oussamajrh <ravenss,com@gmail.com>
2023-07-18 17:50:52 +0100

    up date

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - _putchar alphabet by codex0
+*/
+void print_alphabet(void){
+    char c = 'a';
+        while (c <= 'z')
+        {
+            _putchar(c);
+            c++;
+        }
+        _putchar('\n');   
+}
