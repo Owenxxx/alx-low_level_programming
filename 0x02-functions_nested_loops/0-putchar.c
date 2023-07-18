9f968a2af372d4cdab805e58fb7eee72271ad87e
oussamajrh <ravenss,com@gmail.com>
2023-07-18 16:04:55 +0100

    0-putchar.c project codeX0-beep

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ *main - Prints "codex0" followed by a new line.
+ *return always 0.
+ */
+int (void)
+{
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
+	putchar("\n")
+	return (0);
+}
