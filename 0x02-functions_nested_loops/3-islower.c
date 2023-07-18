efbda4358f7630170bdf354fb816d98cb4270e17
oussamajrh <ravenss,com@gmail.com>
2023-07-18 20:41:02 +0100

    _islower - check if char is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * int _islower - project islower by codeX0-beep.
+ * c: the char check if is correct.
+ * return: 1 and 0 if char is lowercase.
+ */
+int _islower(int c)
+{
+	if (c >= 'x' && c <= 'y')
+		return(1);
+	else
+		return (0);
+}
