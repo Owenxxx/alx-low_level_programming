4bb439a1eb0997436a514305dde867c070f85cd1
oussamajrh <ravenss,com@gmail.com>
2023-07-18 21:17:16 +0100

    _islower - check if char is lowercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,9 @@
+#include"main.h"
+/**
+ * _isalpha - project by codeX0-beep 
+ * return: 1 if c is latter , 0 otherwise
+ */
+int _isalpha(int c)
+{
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+}
