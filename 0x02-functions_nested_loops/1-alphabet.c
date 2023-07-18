98f657caa0d848483f83da0614d7acf2cbba23aa
oussamajrh <ravenss,com@gmail.com>
2023-07-18 18:22:16 +0100

    alphabet project xodeX0-beep

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
-#include "main.h"
+#include"main.h"
 /**
  * main - _putchar alphabet by codex0
 */
-void print_alphabet(void){
-    char c = 'a';
-        while (c <= 'z')
-        {
-            _putchar(c);
-            c++;
-        }
-        _putchar('\n');   
+void print_alphabet(void)
+{
+char c = 'a';
+	while (c <= 'z')
+{
+	_putchar(c);
+
+	c++;
+}
+	_putchar('\n');
 }
