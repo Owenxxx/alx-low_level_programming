cd8cf7cfca484033b16765cc4ee9b4965cd45ec2
oussamajrh <ravenss,com@gmail.com>
2023-07-18 19:17:38 +0100

    up date xodeX0-beep

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,22 @@
 #include "main.h"
 /**
- 
-print_alphabet_x10 - a function that prints 10 times the alphabet*
-Return: x10 a-z
+* print_alphabet_x10 - a function that prints 10 times the alphabet
+* Return: return always 0.
 */
 void print_alphabet_x10(void)
 {
-    int co;
-    char n;
+	int co;
+	char n;
 
-    co = 0;
+	co = 0;
 
-    while (co < 10)
-    {
-        for (n = 'a'; n <= 'z'; n++)
-        {
-            _putchar(n);
-        }
-        co++;
-        _putchar('\n');
-    }
+	while (co < 10)
+{
+	for (n = 'a'; n <= 'z'; n++)
+{
+	_putchar(n);
+}
+	co++;
+	_putchar('\n');
+}
 }
