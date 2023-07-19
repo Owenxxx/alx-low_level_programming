9940c3d271c1fae864629b3fa68ad451838ffa42
oussamajrh <ravenss,com@gmail.com>
2023-07-19 01:14:03 +0100

    codex0-up date

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,28 +1,16 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
+ * add - adds two integers and returns the result
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of a and b
  */
-void jack_bauer(void)
+int add(int a, int b)
 {
-	int i, j;
+	int c;
 
-	i = 0;
+	c = a + b;
 
-	while (i < 24)
-	{
-		j = 0;
-		while (j < 60)
-		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
-			_putchar('\n');
-			j++;
-		}
-		i++;
-	}
+	return (c);
 }
