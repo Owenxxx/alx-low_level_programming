4fe8c38587e19b01fb7e2303ec3388aea967ade4
oussamajrh <ravenss,com@gmail.com>
2023-07-20 17:28:27 +0100

    project 0x04 from codex0-beep

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,18 +3,12 @@
 /**
  * _isupper - uppercase letters
  * @c: char to check
- *
  * Return: 0 or 1
  */
 
 int _isupper(int c)
 {
-	if (c >= 'A')
+	return (c >= 'A' && c <= 'A');
 
-		return (1);
-
-	if else(c <= 'A')
-
-		return (0);
 }
 
