e0c7e713e062ae8b6ead54b37802392d3d360d19
oussamajrh <ravenss,com@gmail.com>
2023-08-02 19:51:08 +0100

    codex test.

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	char s[20] = Puts with recursion;
+		printf("%c",c);
+		return (0);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+project 0x08 by codex0-beep
