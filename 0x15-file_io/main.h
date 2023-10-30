dcff997886abedae3dfa74c99c77213102c93c1e
codex0-beep <ravenss.com@gmail.com>
2023-10-30 20:02:25 +0100

    codexbeep is here babe []

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,7 +3,7 @@
 
 /*
  * File: main.h
- * by: noisefunny
+ * by: codexbeep
  */
 
 #include <stdio.h>
@@ -17,6 +17,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
+int append_text_:wq!
+lsto_file(const char *filename, char *text_content);
 
 #endif/*MAIN_H*/
